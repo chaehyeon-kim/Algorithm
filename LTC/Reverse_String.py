a5d7ff0037c928bd5247b216e72020a25d849a33
chaehyeon-kim <7chaeny25@gmail.com>
2022-04-07 23:08:26 +0900

    [LTC] Easy / Strings

--- LTC/Reverse_String.py
+++ LTC/Reverse_String.py
@@ -1,6 +1,6 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        left, right = 0, len(s) -1
+        left, right = 0, len(s) - 1
         while left < right:
             s[left], s[right] = s[right], s[left]
             left += 1
