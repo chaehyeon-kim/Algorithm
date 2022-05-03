f736d916c642cbf4d90bf5f1136e37a343c5e1d2
chaehyeon-kim <64060975+chaehyeon-kim@users.noreply.github.com>
2022-05-03 12:36:56 +0900

    [LTC] Easy / Array

---
+++ LTC/Rotate_Array.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
